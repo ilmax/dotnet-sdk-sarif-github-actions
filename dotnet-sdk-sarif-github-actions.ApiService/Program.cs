3d2413973aa36ff631c48afdf7bd8aff66e37951
Massimiliano Donini <massimiliano.donini@gmail.com>
2024-09-05 10:45:09 +0200

    Fix violations

--- dotnet-sdk-sarif-github-actions.ApiService/Program.cs
+++ dotnet-sdk-sarif-github-actions.ApiService/Program.cs
@@ -16,14 +16,16 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", async () =>
+app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+#pragma warning disable CA5394            
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
+#pragma warning restore CA5394             
         ))
         .ToArray();
     return forecast;
